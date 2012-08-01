56c436483f5f82b9050cb1ce6822a4fea0d20e63
Vitor <vitorlara@fisica.if.uff.br>
2012-08-01 12:13:44 -0300

    second commit

---
+++ newman_ziff/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  newman_ziff
+//
+//  Created by vitor on 01/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
